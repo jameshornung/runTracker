1f818239f66b856ea0afe122b3676697187c1064
James Hornung <hornung.james@gmail.com>
2016-06-24 16:59:50 -0500

    slight updates to profile page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,14 @@
-console.log('attached');
+// var activeKey = '&api_key=tts32rcx99fc2hps6ca5yrt8';
+// var city='austin';
+// var state='TX';
+// var event='marathon';
+// var start='2016-01-01..';
+// var queryURL = 'http://api.amp.active.com/search?&city='+city+'&state='+state+'&query='+event+'&start_date='+start+activeKey;
+
+
+
+// $.ajax({url: queryURL, method: 'GET', dataType: 'jsonp', crossDomain: 'true'}).done(function(response){
+// 	console.log(response);
+// });
+
+var raceInfo = new Firebase('https://raceinfo.firebaseio.com/');
---
+++ even.js
@@ -0,0 +1,10 @@
+var firstNum = parseInt(process.argv[2]);
+var secondNum = parseInt(process.argv[3]);
+
+
+if(firstNum === secondNum){
+	console.log('true');
+}
+else{
+	console.log('false');
+}
